6dbf6e0241eb3089a2456e1ed5c9a2b444b13634
Vinay <vinay.s@imversion.com>
2025-12-29 13:55:04 +0530

    Not use Recommended Products

--- src/features/recommended-products/containers/recommended-products-container.tsx
+++ src/features/recommended-products/containers/recommended-products-container.tsx
@@ -1,20 +1,20 @@
 import { DEFAULT_PAGE_SIZE } from "@/consts/performance";
 import React from "react";
 
-import { useRecommendedProductsQuery } from "@/graphql/generated/graphql";
+// import { useRecommendedProductsQuery } from "@/graphql/generated/graphql";
 
 export function RecommendedProductsContainer() {
-  const {
-    data: recommendedProducts,
-    loading: isLoading,
-    error,
-  } = useRecommendedProductsQuery({
-    variables: {
-      limit: DEFAULT_PAGE_SIZE,
-    },
-  });
+  // const {
+  //   data: recommendedProducts,
+  //   loading: isLoading,
+  //   error,
+  // } = useRecommendedProductsQuery({
+  //   variables: {
+  //     limit: DEFAULT_PAGE_SIZE,
+  //   },
+  // });
 
-  if (isLoading) return null;
+  // if (isLoading) return null;
 
   return <div>RecommendedProductsContainer</div>;
 }
